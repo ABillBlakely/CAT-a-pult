a9a19a3fd97327695f97b937f24b1067b282246f
Bill Blakely <ABillBlakely@gmail.com>
2016-11-27 18:29:31 -0800

    Test routine for LCD Display driver.

--- catapult.c
+++ catapult.c
@@ -3,24 +3,28 @@
 
 int main(void)
 {
-    uint8_t current_btn = 0;
+    char letter = 'a';
 
     initBtns();
     initDebug();
+    initLCD();
 
     sei();
 
     while(1)
     {
-        current_btn = getBtnFlag();
-        switch (current_btn)
+        toggleLED();
+        if (letter <= 'f')
         {
-            case 1: toggleLED(); break;
-            case 2: toggleLED(); break;
-            case 3: toggleLED(); break;
-            case 4: toggleLED(); break;
-            default: break;
+            LCDWriteLetter(letter);
+            letter++;
         }
+        else
+        {
+            LCDClearAndHome();
+            letter = 'a';
+        }
+        _delay_ms(1000);
     }
     return 0;
 }
